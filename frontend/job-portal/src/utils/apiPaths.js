b0b0d537ab38cef5cbfd2e2288a21fe2c8610f40
Hung-1210 <hungnd.22it@vku.udn.vn>
2025-10-18 22:36:47 +0700

    Update link backend

--- frontend/job-portal/src/utils/apiPaths.js
+++ frontend/job-portal/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+export const BASE_URL =  "https://job-portal-b701.onrender.com/";
 
 export const API_PATHS = {
   AUTH: {
